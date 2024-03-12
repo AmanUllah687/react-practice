429c667b0faff872314786858e1148ec6857afeb
AmanUllah687 <Amanch687@gmail.com>
2024-03-12 14:31:12 +0500

    Added components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import Heading from "./Heading";
 function App() {
     return (
@@ -7,4 +8,46 @@ function App() {
         </div>
     );
 };
+export default App;
+*/
+
+// Experssions as props 
+/*
+const bool = false;
+const str1 = "just";
+function Example(props) {
+    return (
+        <div>
+        <h2>The Value of toggleBoolean prop is: {props.toggleBoolean.toString()}</h2>
+        <p>The Value of math props is: <em>{props.math}</em></p>
+        <p>The Value of str props is: <em>{props.str}</em></p>
+        </div>
+    );
+}; 
+export default function App () {
+    return (
+        <div className="App">
+            <Example toggleBoolean={!bool}
+            math={(10+20)/3}
+            str={`str1 + ' another ' + 'string'`} 
+            />
+        </div>
+    );
+};
+*/
+
+// Embedding in attributes 
+import avatar from './avatar.png'
+function Logo(props) {
+    const userPic = <image src={avatar} />;
+    return userPic;
+};
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+            <Logo />
+        </div>
+    );
+}
 export default App;
