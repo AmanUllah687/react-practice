d9b42f62ff6ccd915e0cca6f7c657195c72f806c
AmanUllah687 <Amanch687@gmail.com>
2024-03-12 11:33:50 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Heading from "./Heading";
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Any other name then Bob" />
+            <Heading firstName="Jack" />
+        </div>
+    );
+};
+export default App;
---
+++ src/Apples.js
@@ -0,0 +1,13 @@
+function Apples(props) {
+    <div className="promo-section">
+        <div>
+            <h2>These apples are: {props.color}</h2>
+            </div>
+            <div>
+            <h2>There are {props.nunmber} apples.</h2>
+            </div>
+    </div>
+
+}
+
+export default Apples;
