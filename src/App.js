233f1a04ccaf8f48051df0a113daa10586701468
AmanUllah687 <Amanch687@gmail.com>
2024-03-16 12:24:48 +0500

    Added Using hooks example 2

--- src/App.js
+++ src/App.js
@@ -180,3 +180,50 @@ export default function InputComponent() {
   )
 }
 */
+import { useState } from "react";
+export default function RegisterForm() {
+  const [form, setForm] = useState({
+  firstName: 'Luke',
+  lastName: 'Jones',
+  email: 'lukejones@sculputure.com'
+  });
+  return (
+    <>
+    <label>
+      First Name:
+      <input value={form.firstName} onChange={e =>{
+        setForm({
+          ...form,
+          firstName: e.target.value
+        });
+      }}
+      />
+    </label>
+    <label>
+    Last name:
+    <input value={form.lastName}  onChange={e => {
+      setForm({
+        ...form,
+        lastName: e.target.value
+      });
+    }}
+    />
+    </label>
+    <label>
+      Email:
+      <input value={form.email} onChange={e => {
+        setForm({
+          ...form,
+          email: e.target.value
+        });
+      }}
+      />
+    </label>
+    <p>
+      {form.firstName} {''}
+      {form.lastName}{''}
+      {form.email}
+    </p>
+    </>
+  );
+}
