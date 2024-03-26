1ffcf291e5ee4a4f7367085defe3b60406d388d5
AmanUllah687 <Amanch687@gmail.com>
2024-03-26 14:19:34 +0500

    added conditional components

--- src/App.js
+++ src/App.js
@@ -413,6 +413,7 @@ function CurrentMessage({day}) {
   }
 export default CurrentMessage;
 */
+/*
 function LogicalAndExample() {
   const val = prompt('Anything but 0');
   return (
@@ -425,3 +426,36 @@ function LogicalAndExample() {
 export default LogicalAndExample;
 true && console.log('This will show');
 false && console.log('This will never show');
+*/
+// Conditional components 
+function App() {
+  const time = new Date();
+  const day = time.toLocaleString("en-us", {weekday: "long"});
+  const morning = time.getHours() >= 6 && time.getHours <= 12;
+  let dayMessage;
+  if(day.toLowerCase() === "monday") {
+    dayMessage = `Happy ${day}`;
+  } else if(day.toLowerCase() === "tuesday") {
+    dayMessage = `${day}, Four days to go`;
+  }
+  else if(day.toLowerCase() === "wednessday")
+  {
+   dayMessage = `${day}, Half way there.`;
+  }
+  else if(day.toLowerCase() === "thursday") {
+    dayMessage = `${day}, Start Planing the Weekend`;
+  }
+  else if(day.toLowerCase() === "friday") {
+    dayMessage = `wo - ho, Weekend is Coming!`;
+  }
+  else {
+    dayMessage = "Stay calm and keep having fun"
+  }
+  return (
+    <div className="App">
+      <h1>{dayMessage}</h1>
+      {morning ? <h2>Have you had a breakfast yet!</h2> : ''}
+    </div>
+  );
+} 
+export default App;
